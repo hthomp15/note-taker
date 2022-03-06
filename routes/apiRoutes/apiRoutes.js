63d66d46ee9e26ebce4b54206a25a7a4142e199e
hthomp15 <hailethomp@gmail.com>
2022-03-06 17:52:35 -0500

    connect to front end and rearrane file paths

---
+++ routes/apiRoutes/apiRoutes.js
@@ -0,0 +1,42 @@
+
+const router = require('express').Router();
+const uniqid = require('uniqid');
+
+const { validateNote, createNote, deleteNote } = require('../../lib/notes')
+const { notes } = require('../../db/db.json')
+
+const id = uniqid()
+
+router.get('/notes', (req, res) => {
+    res.json(notes);
+})
+
+router.post('/notes', (req, res) => {
+    req.body.id = uniqid()
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.')
+    } else {
+        const note = createNote(req.body, notes)
+        res.json(note);
+    }
+});
+
+// router.get('/notes/:id'), (req, res) => {
+//     const noteID = req.params.id
+
+//    const result = notes.filter(note => note.noteid === noteID); 
+//    return res.json(result)
+// }
+// router.delete('/notes/:id', (req, res) => {
+//     const { id } = request.params;
+//     const deletedNote = notes.find(note => note.id === id);
+    
+//     if(!deletedNote) {
+//         res.status(404).send('The note cound not be deleted. The ID was not found.')
+//     } else {
+//         notes = notes.filter(note => note.id !== id);
+//         res.status(200).json(deleteNote)
+//     }
+// });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,54 +1,26 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const uniqid = require('uniqid');
+
+const apiRoutes = require('./routes/apiRoutes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes/htmlRoutes');
 const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-const { notes } = require('./Develop/db/db.json')
 
-const id = uniqid();
 
-function createNote(body, noteArray) {
-    console.log(body);
-    noteArray.push(body)
-    fs.writeFileSync(
-        path.join(__dirname, './Develop/db/db.json'),
-        JSON.stringify( { notes: noteArray }, null, 2)
-    )
-    return body;
-}
 
-function validateNote(note) {
-    if (!note.title || typeof note.title !== 'string'){
-        return false;
-    }
-    if (!note.text || typeof note.text !== 'string'){
-        return false;
-    }
-    if (!note.id || typeof note.id !== 'string'){
-        return false;
-    };
-    return true;
-}
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-})
 
-app.post('/api/notes', (req, res) => {
-    req.body.id = uniqid()
-    if (!validateNote(req.body)) {
-        res.status(400).send('The note is not properly formatted.')
-    } else {
-        const note = createNote(req.body, notes)
-        res.json(note);
-    }
-});
-
-app.listen(3001, () => {
-    console.log('API server on PORT 3001')
+app.listen(PORT, () => {
+    console.log(`API server on PORT ${PORT}`)
 })
